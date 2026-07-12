513e8cb731bb4f9827768dcdcf528f3f2c88c890
cdelriodepaz <cdelriodepaz@gmail.com>
2026-07-12 22:20:02 +0200

    feat: add Publication views for community announcements list and detail

--- comunidad/urls.py
+++ comunidad/urls.py
@@ -4,4 +4,12 @@ from . import views
 app_name = "comunidad"
 urlpatterns = [
     path("<slug:slug>/", views.pagina_comunidad, name="pagina_comunidad"),
+    path(
+        "<slug:slug>/avisos/", views.community_publications, name="pagina_publicaciones"
+    ),
+    path(
+        "<slug:slug>/avisos/<int:publication_id>/",
+        views.community_publication_closeup,
+        name="pagina_publicacion",
+    ),
 ]
--- comunidad/views.py
+++ comunidad/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Parish
+from .models import Parish, Publication
 
 
 # Create your views here.
 def pagina_comunidad(request, slug):
     parish = get_object_or_404(Parish, slug=slug)
     return render(request, "comunidad/pagina_comunidad.html", {"parish": parish})
+
+
+def community_publications(request, slug):
+    parish = get_object_or_404(Parish, slug=slug)
+    publications = Publication.objects.filter(parish=parish, published=True)
+    return render(
+        request, "comunidad/community_publications.html", {"publications": publications}
+    )
+
+
+def community_publication_closeup(request, slug, id):
+    parish = get_object_or_404(Parish, slug=slug)
+    publication = get_object_or_404(Publication, id=id)
+    return render(
+        request,
+        "comunidad/community_publications_closeup.html",
+        {"publication": publication},
+    )
