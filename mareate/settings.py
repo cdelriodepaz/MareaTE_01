02c24c84e2674bddee80ed5d0926af9a7549eece
cdelriodepaz <cdelriodepaz@gmail.com>
2026-07-13 16:16:44 +0200

    feat: move SECRET_KEY and DEBUG to environment variables with python-decouple

--- mareate/settings.py
+++ mareate/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-409b6&0#jv2u)^nq!y7**!nqp^xekh068%)=$8t3fy+gu@_ceu"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
